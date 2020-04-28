26bda9a432a4f71b7cd4c2e23df46addc722572b
itstc <itsthomaschu@gmail.com>
2020-04-28 15:41:24 -0600

    setup docker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
+import io from 'socket.io-client';
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {UnControlled as CodeMirror} from 'react-codemirror2'
 
-const App = () => {
-  return <h1>Hello World!!!</h1>;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.socket = io('http://localhost:3000');
+
+    this.state = {
+      text: ''
+    }
+
+    this.handleEditorChange = this.handleEditorChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.socket.on('text', (data) => {
+      this.setState({text: data.text});
+    });
+  }
+
+  handleEditorChange(event) {
+    console.log('value: ' + event.target.value);
+    this.socket.emit('changeText', {text: event.target.value});
+  }
+
+  render() {
+    return (<CodeMirror
+      value={this.state.text}
+      options={{
+        mode: 'javascript',
+        theme: 'material',
+        lineNumbers: true
+      }}
+      onChange={(editor, data, value) => {
+        console.log('value: ' + value);
+        if (value !== this.state.text) {
+          this.socket.emit('changeText', {text: value});
+        }
+      }}
+    />);
+  }
 }
 
 ReactDOM.render(<App/>, document.getElementById("app"));
